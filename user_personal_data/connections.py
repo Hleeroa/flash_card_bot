dea26d65eeb6c213630752342c13c8d383119195
GeraLeo <gerakalasnikova@gmail.com>
2024-06-24 15:43:27 +0500

    img refactored

--- user_personal_data/connections.py
+++ user_personal_data/connections.py
@@ -1,9 +1,8 @@
 from telebot.storage import StateMemoryStorage
 
-
-database = 'Deu-Eng'
-user = "postgres"
-password = 'blah123'  # Здесь хранится инфорбация о базе данных
+database = 'Deu-Eng'  #
+user = "postgres"  # Здесь хранится инфорбация о базе данных
+password = 'blah123'  #
 
 state_storage = StateMemoryStorage()
 token_bot = '6808634678:AAGYJai0b8pLGRI-XiBmrhsIwiCkFWhnguw'  # Здесь хранится Токен бота
