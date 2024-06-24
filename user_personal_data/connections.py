848c9c324e1095ce9c160cc59ffaf34d85f37fd1
Hleeroa <153171612+Hleeroa@users.noreply.github.com>
2024-06-24 21:42:52 +0500

    connections.py

--- user_personal_data/connections.py
+++ user_personal_data/connections.py
@@ -2,4 +2,4 @@ database = 'Deu-Eng'        #
 user = "postgres"           # Здесь хранится информация о базе данных
 password = 'your_password'  #
 
-token_bot = '6808634678:AAGYJai0b8pLGRI-XiBmrhsIwiCkFWhnguw'  # Здесь хранится Токен бота
+token_bot = ''  # Здесь хранится Токен бота
